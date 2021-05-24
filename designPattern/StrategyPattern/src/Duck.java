e0d4ee023915c840f3e115e5462ab85592b0ca54
gwang920 <gwang92@naver.com>
2021-05-24 22:45:22 +0900

    add setter

--- designPattern/StrategyPattern/src/Duck.java
+++ designPattern/StrategyPattern/src/Duck.java
@@ -19,4 +19,16 @@ public abstract class Duck{
     public void swim(){
         System.out.println("모든 오리는 물에뜬다.");
     }
+
+
+    // 오리의 행동을 바꿔주고 싶다면, 아래 두 메소드를 호출한다
+
+    public void setFlyBehavior(FlyBehavior fb){
+        flyBehavior=fb;
+    }
+
+    public void setQuackBehavior(QuackBehavior qb){
+        quackBehavior=qb;
+    }
+
 }
